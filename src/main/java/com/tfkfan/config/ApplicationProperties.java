37853a8e7f0bf5b05d907477633b7178abdb2451
tfkfan <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>
2022-04-04 17:47:03 +0300

    initialized with jhipster

---
+++ src/main/java/com/tfkfan/config/ApplicationProperties.java
@@ -0,0 +1,12 @@
+package com.tfkfan.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Test Soap Service.
+ * <p>
+ * Properties are configured in the {@code application.yml} file.
+ * See {@link tech.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {}
---
+++ src/test/java/com/tfkfan/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.tfkfan;
+
+import com.tfkfan.TestSoapServiceApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = TestSoapServiceApp.class)
+public @interface IntegrationTest {
+}
